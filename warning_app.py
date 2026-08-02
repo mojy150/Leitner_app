e263b504c36a6fa6f856d3b6d056d6d38eec42a0
mojy150 <mojy429@gmail.com>
2026-08-02 15:52:41 +0330

    Adding checkbox to the edit section

--- warning_app.py
+++ warning_app.py
@@ -3,7 +3,8 @@ from variable import *
 
 def warning_app1(parent,en_text,fr_text,Table_name):
     warning_app = CTkToplevel(parent)
-    warning_app.title("Warning")
+    warning_app.title("Edit FlashCard")
+    warning_app.geometry("300x400")
     warning_app.grab_set()
 
     warning_app.after(10, warning_app.lift)
@@ -13,9 +14,9 @@ def warning_app1(parent,en_text,fr_text,Table_name):
 
     warning_app.grid_rowconfigure(0, weight=1)
     warning_app.grid_rowconfigure(1, weight=1)
-    warning_app.grid_rowconfigure(2, weight=1)
-    warning_app.grid_rowconfigure(3, weight=8)
-    warning_app.grid_rowconfigure([4,5,6], weight=1)
+    warning_app.grid_rowconfigure(2, weight=0)
+    warning_app.grid_rowconfigure(3, weight=1)
+    # warning_app.grid_rowconfigure([4,5,6], weight=1)
 
     en_input = CTkEntry(warning_app,                          
                         placeholder_text="english: ",
@@ -43,15 +44,31 @@ def warning_app1(parent,en_text,fr_text,Table_name):
 
     fr_input.insert(0, fr_text)
 
+    day_1th_checkbox = CTkCheckBox(warning_app,text="day 1th ?",       
+                        onvalue="on",                      
+                        offvalue="off",                 
+                        font=en_font,                      
+                        variable=StringVar(value="off"),
+                        )              
+    day_1th_checkbox.grid(column=0,row=2,padx=10,pady=10)  
+
     def add_the_word():
         if en_input.get().strip() != "" and fr_input.get().strip() != "":
             text_en_input= en_input.get().strip()
             text_fr_input= fr_input.get().strip()
-            cursor.execute(f"UPDATE {Table_name} SET answer=? WHERE question=?",
-                               (text_fr_input,
-                                text_en_input
-                                ))
-            conn.commit()
+            if day_1th_checkbox.get() != "on":
+                cursor.execute(f"UPDATE {Table_name} SET answer=? WHERE question=?",
+                                    (text_fr_input,
+                                    text_en_input
+                                    ))
+                conn.commit()
+            else:
+                cursor.execute(f"UPDATE {Table_name} SET answer=?, day=? WHERE question=?",
+                                                    (text_fr_input,
+                                                    1,
+                                                    text_en_input,
+                                                    ))
+                conn.commit()
 
             en_input.delete(0,END)
             fr_input.delete(0,END)
@@ -63,4 +80,4 @@ def warning_app1(parent,en_text,fr_text,Table_name):
                             text="edit your Flashcard",
                             font=en_font,
                             command=add_the_word)
-    add_word_btn.grid(column=0,row=2,sticky='nsew',padx=10,pady=10)
+    add_word_btn.grid(column=0,row=3,sticky='nsew',padx=10,pady=10)
